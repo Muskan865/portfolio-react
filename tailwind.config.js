78e21c80a1ffd5ccf5727a8399a8cf9688ec77a7
Muskan <muskanrehan865@gmail.com>
2024-08-08 22:31:27 +0500

    added project to git

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        custom: {
+          500: '#C63C51', // Define your custom color here
+        },
+      },
+    },
+  },
+  plugins: [],
+};
